2c3c1db1c7e7c3989e0d295e1e6faa80b74d255f
Waidroc <92311074+Waidroc@users.noreply.github.com>
2025-01-11 20:29:35 +0100

    Update detectar_nuevos_hosts.sh

--- scripts/detectar_nuevos_hosts.sh
+++ scripts/detectar_nuevos_hosts.sh
@@ -5,8 +5,8 @@ CONFIG_FILE="/home/waidroc/Tools/periodicNetworkDiscovery/configs/redes_a_monito
 KNOWN_HOSTS_DIR="/home/waidroc/Tools/periodicNetworkDiscovery/output/known_hosts"
 KNOWN_DEVICES_FILE="/home/waidroc/Tools/periodicNetworkDiscovery/output/dispositivos_conocidos.txt"
 LOG_DIR="/home/waidroc/Tools/periodicNetworkDiscovery/output/logs"
-BOT_TOKEN="7710350394:AAFX9WI9wXHXaEVUJLlwSzW12WkbJOkfUYg"
-CHAT_ID="6965910510"
+BOT_TOKEN="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CHAT_ID="xxxxxxxxxx"
 
 # Crear directorios y archivo de dispositivos conocidos si no existen
 mkdir -p $KNOWN_HOSTS_DIR $LOG_DIR
